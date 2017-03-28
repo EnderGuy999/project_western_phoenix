898b8eff7f8e5ca54791f42a2d613db76902000c
Robert <robertg@crabtreemail.net>
2017-03-27 19:40:43 -0500

    Begun Work
    Made bace code for character.py and weapons.py, and starting work on logic.py.

---
+++ logic.py
@@ -0,0 +1,16 @@
+from character import Player
+from weapons import Weapon
+import random
+
+wep_array = ()
+
+rifle = Weapon("rifle")
+
+
+
+def chance(percent=50):
+    return random.randrange(100) < percent
+
+
+def game_init():
+    p = Player()
---
+++ user-input.py
@@ -0,0 +1,6 @@
+import logic
+
+def user_response():
+    user_input = input().lower()
+
+    if user_input == "attack":
